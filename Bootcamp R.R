2d4446787b11b83b6536955d68f5e7f8e3b8a45f
PierDeRogatis <97444678+PierDeRogatis@users.noreply.github.com>
2022-01-10 11:25:31 +0000

    Add files via upload

---
+++ Bootcamp R.R
@@ -0,0 +1,52 @@
+# DATA SCIENCE BOOTCAMP: R ######################################################
+
+## DAY ONE: Introduction to R ==============================================================
+
+### 1.1 ---------------------------------------------------------------------------------------
+
+# Create your GitHub profile and first repository Introduction2R
+
+
+### 1.2 ------------------------------------------------------------------------------------------
+
+# Create your first R scripts using R-Studio: File -> New File -> R Script and name it.
+
+
+### 1.3 ------------------------------------------------------------------------------------------
+
+# Simply try to add two numbers and print your answer
+
+a1 <- 675 + 32
+
+print(a1)
+
+
+### 1.4 -----------------------------------------------------------------------------------------
+
+# Now try to subtract 5 from it and print your answer
+
+a2 <- a1 - 5
+
+print(a2)
+
+
+### 1.5 -----------------------------------------------------------------------------------------
+
+# Now multiply the answer with 3 using * operator and print the answer.
+
+a3 <- a2*3
+
+print(a3)
+
+
+### 1.6 ------------------------------------------------------------------------------------------
+
+# Now divide the number by 2 using / operator.
+
+a4 <- a3/2
+
+print(a4)
+
+
+## DAY TWO - Syntax and Data Structures ============================================================
+
